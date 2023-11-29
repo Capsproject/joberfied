6510ede26c4aaa20779ba7f1b9cd65c4fa1462f7
RMan0 <datinguinooromano@gmail.com>
2023-11-29 14:45:41 +0800

    remove add employee button

--- admin/employee/list.php
+++ admin/employee/list.php
@@ -6,7 +6,7 @@
 
 <div class="row">
 	<div class="col-lg-12">
-		<h1 class="page-header">List of Employees <a href="index.php?view=add" class="btn btn-primary btn-xs"> <i class="fa fa-plus-circle fw-fa"></i> Add New Employee</a> </h1>
+		<h1 class="page-header">List of Employees  </h1>
 	</div>
 	<!-- /.col-lg-12 -->
 </div>
